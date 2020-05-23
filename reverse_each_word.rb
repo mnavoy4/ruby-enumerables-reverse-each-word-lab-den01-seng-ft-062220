b473864898b60ad8694b0b30eccf57b8a360e60f
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-23 12:51:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,19 @@
-def reverse_each_word(string)
-  array = string.split
-  row_index = 0
-  reversed_array = []
-  while row_index < array.length do
-    element_index = array[row_index].length - 1
-    while element_index >= 0 do
-      reversed_array.push(array[row_index][element_index])
-      element_index -= 1
-    end
-    row_index += 1 
-  end
-  p reversed_array
-end
+# def reverse_each_word(string)
+#   array = string.split
+#   row_index = 0
+#   reversed_array = []
+#   while row_index < array.length do
+#     element_index = array[row_index].length - 1
+#     while element_index >= 0 do
+#       reversed_array.push(array[row_index][element_index])
+#       element_index -= 1
+#     end
+#     row_index += 1 
+#   end
+#   p reversed_array
+# end
 
-reverse_each_word("Hello there")
+# reverse_each_word("Hello there")
 
 
   
