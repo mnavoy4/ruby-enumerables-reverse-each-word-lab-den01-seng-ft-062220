22c9c0c3b9b224a14f1a24b7d822455f6df99758
dazzling-archive-8607 <mnavoy4@gmail.com>
2020-05-23 12:43:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   row_index = 0
   reversed_array = []
   while row_index < array.length do
-    element_index = row_index.length - 1
+    element_index = array[row_index].length - 1
     while element_index <= 0 do
       reversed_array.push(array[row_index][element_index])
       element_index -= 1
